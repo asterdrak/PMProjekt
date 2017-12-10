161c4ce8489828580193e275b62b9d82fd7ae0c0
Roger Woźniak <giny21@gmail.com>
2017-12-10 13:25:56 +0100

    MOSCOW button methods

--- app/src/main/java/rustrll/sl/com/russiantrello/TaskAdapter.java
+++ app/src/main/java/rustrll/sl/com/russiantrello/TaskAdapter.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -15,10 +16,11 @@ public class TaskAdapter extends ArrayAdapter<Task> {
         super(context, 0, tasks);
     }
 
+    public Task task;
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         // Get the data item for this position
-        Task task = getItem(position);
+        task = getItem(position);
 
         // Check if an existing view is being reused, otherwise inflate the view
         if (convertView == null) {
@@ -35,6 +37,38 @@ public class TaskAdapter extends ArrayAdapter<Task> {
         // Populate the data into the template view using the data object
         taskName.setText(task.getName());
         taskDueDate.setText(task.getDueDate());
+
+        Button mustButton = (Button) convertView.findViewById(R.id.must);
+        mustButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                task.setMoscow("must");
+            }
+        });
+
+        Button couldButton = (Button) convertView.findViewById(R.id.could);
+        mustButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                task.setMoscow("could");
+            }
+        });
+
+        Button shouldButton = (Button) convertView.findViewById(R.id.should);
+        mustButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                task.setMoscow("should");
+            }
+        });
+
+        Button wontButton = (Button) convertView.findViewById(R.id.wont);
+        mustButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                task.setMoscow("wont");
+            }
+        });
 
         // Return the completed view to render on screen
         return convertView;
